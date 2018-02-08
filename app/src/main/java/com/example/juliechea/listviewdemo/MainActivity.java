18911990de1db8f064864f7707b08711968adf7a
Julie Chea <juliechea@Julies-MacBook-Pro.local>
2018-02-08 18:17:09 -0500

    Initial commit

---
+++ app/src/main/java/com/example/juliechea/listviewdemo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.juliechea.listviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.class_schedule);
+
+        String[] classes = {"220", "221", "395", "495", "490"};
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, classes);
+        listView.setAdapter(adapter);
+
+     listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+         @Override
+         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+             Toast.makeText(getApplicationContext(), "Short click", Toast.LENGTH_SHORT).show();
+         }
+     });
+
+     listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+         @Override
+         public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+             Toast.makeText(getApplicationContext(), "Looooooooong click", Toast.LENGTH_SHORT).show();
+             //return false;
+
+             return true;
+         }
+     });
+
+    }
+}
